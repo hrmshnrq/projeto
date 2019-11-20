b1615bc20fe432b451ad3286a6eb73162c9b8ecd
hrmshnrq <55238606+hrmshnrq@users.noreply.github.com>
2019-11-19 21:35:24 -0300

    Delete criarUsuario.php

--- criarUsuario.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $a = file_get_contents("../xml/mensagem.xml");
-   $b = simplexml_load_string($a);
-
-   $mensagem["email"] = trim($b->email);
-   $mensagem["nome"] = trim($b->nome);
-   $mensagem["sobrenome"] = trim($b->sobrenome);
-   $mensagem["senha"] = trim($b->senha);
-?>
