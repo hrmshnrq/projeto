c0d1b13e0259d673e49608ceac35695965874615
hrmshnrq <55238606+hrmshnrq@users.noreply.github.com>
2019-11-20 20:44:11 -0300

    Update php.php

--- php/php.php
+++ php/php.php
@@ -1,82 +1,29 @@
 <?php
-    $pessoa = $_POST["pessoa"];
-    $senhadapessoa = $_POST["senhadapessoa"];
-    
-    if($pessoa == "exemplo@gmail.com" && $senhadapessoa == 12345){
-        $retorno["status"] = "s";
-        $retorno["mensagem"] = "usuario autenticado";
-    }
-    
-    else{
-        $retorno["status"] = "n";
-        $retorno["mensagem"] = "usuario nao autenticado";
-    }
-
-    $retorno_json = json_encode($retorno);
-    echo $retorno_json;
-
-
+    // Salvando dados em variáveis php
+    $nome = $_POST["nome"];
+    $sobrenome = $_POST["sobrenome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
 
     $xml = new DOMDocument("1.0");
+    $xml->formatOutput = true;
 
- //estruturas
- $cpf = $_POST["cpf"];
- $mail = $_POST["email"];
- $nome = $_POST["nome"];
- $sobre = $_POST["sobrenome"];
- $senha = $_POST["senha"];
+    // elementos
+    $xml_usuario = $xml->createElement("usuarior");
+    $xml_nome = $xml->createElement("nome", $nome);
+    $xml_sobrenome = $xml->createElement("sobrenome", $sobrenome);
+    $xml_email = $xml->createElement("email", $email);
+    $xml_senha = $xml->createElement("sennha", $senha);
 
- $xml = new DOMDocument("1.0");
+    // crinaças
+    $xml_usuario->appendChild( $xml_nome );
+    $xml_usuario->appendChild( $xml_sobrenome );
+    $xml_usuario->appendChild( $xml_email );
+    $xml_usuario->appendChild( $xml_senha );
 
- $dados = $xml->createElement("dados");
+    $xml->appendChild( $xml_user );
 
- $xml_cpf = $xml->createElement("cpf", $cpf);
- $xml_email = $xml->createElement("email", $mail);
- $xml_nome = $xml->createElement("nome", $nome);
- $xml_sobre = $xml->createElement("sobrenome", $sobre);
- $xml_num = $xml->createElement("senha", $senha);
+    $xml->save("../xml/".$email.".xml");
+    echo json_encode("Conta criada com sucesso!!");
 
- $dados->appendChild($xml_cpf);
- $dados->appendChild($xml_email);
- $dados->appendChild($xml_nome);
- $dados->appendChild($xml_sobre);
- $dados->appendChild($xml_end);
- $dados->appendChild($xml_num);
-
- $xml->appendChild($dados);
-
- $xml->save("dados.xml");
-
- echo json_encode("xml criado");
-
-
-
-// novo usuario, pagina de cadastro
- $xml = new DOMDocument("1.0");
-
- //estruturas
- $xml_dados = $xml->creatElement("dados");
- $xml_conteudoEmail = $xml->createElement("email");
- $xml_senha = $xml->createElement("senha");
- $xml_usuario = $xml->createElement("usuario");
- $xml_nome = $xml->createElement("nome");
-
- //destinatario, titulo e texto recebidos no teclado
- $xml_nome1 = $xml->createElement("nome", $nome);
- $xml_senha = $xml->createElement("senha", $senha);
- $xml_sobrenome = $xml->createElement("sobrenome", $sobrenome);
-
- 
- //criancas
- $xml_usuario->appendChild($xml_conteudoEmail);
- $xml_usuario->appendChild($xml_nome);
- $xml_nome->appendChild($xml_nome1);
- $xml_nome->appendChild($xml_sobrenome);
- $xml_usuario->appendChild($xml_senha);
- $xml_dados->appendChild($usuario);
- $xml_dados->appendChild($nome);
-
- $xml->appendChild($xml_dados);
-
- $xml->save("novousuario.xml");
 ?>
