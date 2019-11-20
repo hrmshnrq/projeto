d3a38e95fb6922b97780607fd8518682d05ead7e
hrmshnrq <55238606+hrmshnrq@users.noreply.github.com>
2019-11-20 19:34:29 -0300

    Update funcionamento.js

--- js/funcionamento.js
+++ js/funcionamento.js
@@ -1,25 +1,3 @@
-
-// salvar em xml o novo usuario
-$(document).ready(function(){
-    $("#salvar").click(function(){
-	
-
-		var email = $("#E-mai").val();
-		var nome = $("#Nome").val();
-		var sobre = $("#Sobrenome").val();
-		var senha = $("#Senha").val();
-
-		$.ajax({
-			type: "POST",
-            url: "../php/php.php",
-			success: function(retorno){
-                
-            }
-	    })  
-    });
-});
-
-
 // usuario e senha suspenso em ajax -> mudar para xml
 $(document).ready(function(){
     $("#botao1").click(function(){
@@ -42,7 +20,7 @@ function validalogin (){
         $.ajax({
 
         type: "POST",
-        url: "php/criarusuario.php",
+        url: "php/login.php",
         dataType: "json",
         data: {
             usuario: lusuario,
@@ -62,23 +40,16 @@ function validalogin (){
     }
 }
 
+//cadastro na pagina de login
 $(document).ready(function(){
     $("#botao2").click(function(){
-        passarpagina1();
+        $(location).attr('href', 'sites/cadastro.html');
+
     });
 });
-function passarpagina1 (){
-    $(location).attr('href', 'sites/cadastro.html');
-}
-
-
-
 // função do botão cancelar em login
 $(document).ready(function(){
     $("#cancelar").click(function(){
-        passarpagina2();
+        $(location).attr('href', '../index.html');
     });
 });
-function passarpagina2 (){
-    $(location).attr('href', '../index.html');
-}
