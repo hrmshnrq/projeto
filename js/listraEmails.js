f8a2d18d4430a23b5453d6597aa41599f31493a1
hrmshnrq <55238606+hrmshnrq@users.noreply.github.com>
2019-11-20 13:12:23 -0300

    Add files via upload

---
+++ js/listraEmails.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	$("#bListar").click(function(){
+
+		$.ajax({
+			type: "POST",
+			dataType: "json",
+			url: "php/listarXML.php",
+			success: function(retorno) {
+
+				var conteudo = "";
+				conteudo += "<table>";
+				conteudo += "<tr>";
+				conteudo += "<td>" + retorno.destinatario + "</td>";
+
+				conteudo += "<td>" + retorno.assunto + "</td>";
+
+				conteudo += "<td>" + retorno.mensagem + "</td>";
+				conteudo += "</tr>";
+
+				conteudo += "</table>";
+
+				$("#divEmail").html(conteudo);
+
+			}
+		});
+
+	});
+
+
+});
