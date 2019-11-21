dc43ed55bed2d6c808f2211bc9293a47b0e64c0e
hrmshnrq <55238606+hrmshnrq@users.noreply.github.com>
2019-11-20 21:29:54 -0300

    Add files via upload

---
+++ php/listarusuario.php
@@ -0,0 +1,14 @@
+<?php
+
+   $email = $_POST["usuario"];
+
+   $a = file_get_contents("../xml/".$email.".xml");
+   $b = simplexml_load_string($a);
+   
+   $retorno["email"] = trim($b->email);
+   $retorno["nome"] = trim($b->nome);
+   $retorno["sobrenome"] = trim($b->sobrenome);
+   $retorno["senha"] = trim($b->senha);
+   
+   echo json_encode($retorno);
+?>
